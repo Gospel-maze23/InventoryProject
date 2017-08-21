f97884a1373f84cca9f598d93d3a6a9d83d9449a
emp-daisy <adajess01@gmail.com>
2017-08-21 23:43:14 +0100

    New proj added

---
+++ DInventory/ProductTbl.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProductTbl] (
+    [ProductID]    INT           NOT NULL,
+    [ProductName]  NVARCHAR (50) NULL,
+    [ProductStock] INT           NULL,
+    [Description]  TEXT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
+
